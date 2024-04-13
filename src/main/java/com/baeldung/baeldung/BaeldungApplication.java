4b10457c917728591ec6744f15889cb91efd49a5
Dherendra Dev <42056549+dev1031@users.noreply.github.com>
2024-04-13 21:30:15 +0530

    Initial Commit 😊

---
+++ src/main/java/com/baeldung/baeldung/BaeldungApplication.java
@@ -0,0 +1,12 @@
+package com.baeldung.baeldung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeldungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaeldungApplication.class, args);
+	}
+}
